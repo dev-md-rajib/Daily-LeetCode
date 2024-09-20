88dede9b2e9335c23981e11606bca6067005d3c1
dev-md-rajib <rajibmiah978@gmail.com>
2024-09-20 23:05:34 +0600

    214. Shortest Palindrome statement added

---
+++ Hashing/214. Shortest Palindrome.py
@@ -0,0 +1,8 @@
+"""
+statement:
+You are given a string s. You can convert s to a 
+palindrome by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+
+"""
