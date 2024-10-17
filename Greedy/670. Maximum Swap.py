73372cf1009c3b3fb0c1a9b97b14dbb0c73bd2c0
dev-md-rajib <rajibmiah978@gmail.com>
2024-10-17 16:54:35 +0600

    670. Maximum Swap

---
+++ Greedy/670. Maximum Swap.py
@@ -0,0 +1,37 @@
+"""
+Statement:
+You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+
+Return the maximum valued number you can get.
+
+Input: num = 2736
+Output: 7236
+"""
+
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        num_str = list(str(num))
+        n = len(num_str)
+        max_digit_index = -1
+        swap_idx_1 = -1
+        swap_idx_2 = -1
+
+        
+        for i in range(n - 1, -1, -1):
+            if max_digit_index == -1 or num_str[i] > num_str[max_digit_index]:
+                max_digit_index = i  
+            elif num_str[i] < num_str[max_digit_index]:
+                swap_idx_1 = i  
+                swap_idx_2 = (
+                    max_digit_index  
+                )
+
+        if swap_idx_1 != -1 and swap_idx_2 != -1:
+            num_str[swap_idx_1], num_str[swap_idx_2] = (
+                num_str[swap_idx_2],
+                num_str[swap_idx_1],
+            )
+
+        return int(
+            "".join(num_str)
+        )  
